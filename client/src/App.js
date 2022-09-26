fd841f19ca9fc99fff8208574a55a2eb72127afc
SenDan07 <dany.alekrmz@gmail.com>
2022-09-26 13:23:43 -0700

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>E Commerce App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
