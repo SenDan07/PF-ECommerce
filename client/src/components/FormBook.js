989252b9525545beec2d1ee1c74c8ea72c87a0c1
soraluz <rijasoag23@hotmail.com>
2022-09-29 15:04:04 -0500

    Form Validado

--- client/src/components/FormBook.js
+++ client/src/components/FormBook.js
@@ -10,24 +10,40 @@ export function validate(input) {
 
     let errors = {};
     let expLetras = /^[A-Za-z]+[A-Za-z\s]*[A-Za-z]$/
-    if (!input.autor) {
-        errors.autor = 'Autor es requerido';
-    } else if (!expLetras.test(input.autor)) {
-        errors.autor = 'Autor es invalido'
+    if (!input.title) {
+        errors.title = 'Titulo es requerido';
+    } else if (!expLetras.test(input.title)) {
+        errors.title = 'Titulo es invalido'
     }
-    if (input.editorial) {
-        if (!expLetras.test(input.editorial)) {
-            errors.editorial = 'Editorial es invalida'
+    if (!input.authors) {
+        errors.authors = 'Autor es requerido';
+    } else if (!expLetras.test(input.authors)) {
+        errors.authors = 'Autor es invalido'
+    }
+    if (input.publisher) {
+        if (!expLetras.test(input.publisher)) {
+            errors.publisher = 'Editorial es invalida'
         }
     }
-    if (!input.precio) {
+     if (input.ISBN) {
+        if (/\D/.test(input.ISBN)) {
+            errors.ISBN = "ISBN debe ser numerico"
+        }
+    }
+    if (input.description) {
+        if (!expLetras.test(input.description)) {
+            errors.description = "Descripcion es invalida"
+        }
+    }
+    if (!input.price) {
         errors.precio = 'precio es requerido';
     }
-    else if (input.precio) {
-        if (input.precio < 0 || /\D/.test(input.precio)) {
-            errors.precio = "Precio no valido"
+    else if (input.price) {
+        if (input.price < 0 || !/^[0-9]+([.][0-9]+)?$/.test(input.price)) {
+            errors.price = "Precio no valido"
         }
     }
+
     return errors;
 }
 
@@ -49,15 +65,14 @@ export default function FormBook() {
     //let countries=useSelector(state=>state.countries)
 
     const [input, setInput] = React.useState({
-        autor: '',
-        editorial: '',
-        precio: 0,
-        edicion: '',
-        imagen: '',
-        tipo: '',
-        isbn: '',
-        categoria: [],
-        descripcion: ''
+        title:'',//texto
+        authors: [],//arreglo
+        publisher: '',//texto
+        ISBN:'',//integer
+        categories: [],//string
+        imageLinks: '',//string
+        description:'',//string
+        price: 0,//float
     })
     const [errors, setErrors] = React.useState({});
 
@@ -66,17 +81,16 @@ export default function FormBook() {
         e.preventDefault()
         // dispatch(createActivity(input))
         setInput({
-            autor: '',
-            editorial: '',
-            precio: 0,
-            edicion: '',
-            imagen: '',
-            tipo: '',
-            isbn: '',
-            categoria: [],
-            descripcion: ''
+            title:'',//texto
+            authors: [],//arreglo
+            publisher: '',//texto
+            ISBN:'',//integer
+            categories: [],//string
+            imageLinks: '',//string
+            description:'',//string
+            price: 0,
         })
-        e.target.name.focus()
+        e.target.title.focus()
         //  let boton= document.getElementById('enviar')
         boton.disabled = true
         //   setTimeout(()=>dispatch(setStatus('')),2000)
@@ -92,17 +106,17 @@ export default function FormBook() {
         }))
     }
     function handleSelect(e) {
-        if (input.categoria.includes(e.target.value)) {
+        if (input.categories.includes(e.target.value)) {
             setInput({
                 ...input,
-                categoria: input.categoria.filter((c) => {
+                categories: input.categories.filter((c) => {
                     return c !== e.target.value
                 })
             })
         } else {
             setInput({
                 ...input,
-                categoria: input.categoria.concat(e.target.value)
+                categories: input.categories.concat(e.target.value)
             })
         }
 
@@ -112,30 +126,26 @@ export default function FormBook() {
         <form onSubmit={(e) => handleSubmit(e)} class="bg-[#a3a3a3] text-white container mx-auto p-20 m-20 rounded-3xl w-1/2">
             <h2 class="text-center text-xl text-[30px] text-black">REGISTRO DE NUEVO LIBRO</h2><br />
             <fieldset class="columns-2 text-[18px]">
+                <label class="block">TITULO: </label>
+                <input type='text' class={errors.title ? 'text-[#dc2626]' : 'text-[#075985]'} name='title' value={input.title} placeholder='Ingrese el Titulo del libro' onChange={(e) => handleChange(e)} autoFocus /><br />
+                {errors.title ? <p class="text-[#dc2626]">{errors.title}</p> : null}<br />
                 <label class="block">AUTOR: </label>
-                <input type='text' class={errors.autor ? 'text-red-600' : 'text-blue-600'} name='autor' value={input.autor} placeholder='Ingrese el nombre del Autor' onChange={(e) => handleChange(e)} autoFocus /><br />
-                {errors.autor ? <p class="text-red-600">{errors.autor}</p> : null}<br />
+                <input type='text' class={errors.authors ? 'text-[#dc2626]' : 'text-[#075985]'} name='authors' value={input.authors} placeholder='Ingrese el nombre del Autor' onChange={(e) => handleChange(e)} /><br />
+                {errors.authors ? <p class="text-[#dc2626]">{errors.authors}</p> : null}<br />
                 <label class="block">EDITORIAL: </label>
-                <input type='text' class={errors.editorial ? 'text-red-600' : 'text-blue-600'} name='editorial' value={input.editorial} placeholder='Editorial' onChange={(e) => handleChange(e)} /><br />
-                {errors.editorial ? <p class="text-red-600">{errors.editorial}</p> : null}<br />
-                <label class="block">PRECIO: </label>
-                <input type='text' class={errors.precio ? 'text-red-600' : 'text-blue-600'} name='precio' value={input.precio} placeholder='Ingrese el precio' onChange={(e) => handleChange(e)} /><br />
-                {errors.precio ? <p class="text-red-600">{errors.precio}</p> : null}<br />
-                <label class="block">EDICION: </label>
-                <input type='text' class={errors.edicion ? 'text-red-600' : 'text-blue-600'} name='edicion' value={input.edicion} placeholder='Ingrese el precio' onChange={(e) => handleChange(e)} /><br />
-                {errors.edicion ? <p class="text-red-600">{errors.edicion}</p> : null}<br /><br /><br />
-                <label class="block">IMAGEN: </label>
-                <input type='text' class={errors.imagen ? 'text-red-600' : 'text-blue-600'} name='imagen' value={input.imagen} placeholder='imagen' onChange={(e) => handleChange(e)} />
-                <input type='button' class="bg-[#9a3412] p-1" onClick={e => { return e }} value='Buscar' /><br />
-                {errors.imagen ? <p class="text-red-600">{errors.imagen}</p> : null}<br />
-                <label class="block">TIPO: </label>
-                <input type='text' class={errors.tipo ? 'text-red-600' : 'text-blue-600'} name='tipo' value={input.tipo} placeholder='tipo' onChange={(e) => handleChange(e)} /><br />
-                {errors.tipo ? <p class="text-red-600">{errors.tipo}</p> : null}<br />
+                <input type='text' class={errors.publisher ? 'text-[#dc2626]' : 'text-[#075985]'} name='publisher' value={input.publisher} placeholder='Editorial' onChange={(e) => handleChange(e)} /><br />
+                {errors.publisher ? <p class="text-[#dc2626]">{errors.publisher}</p> : null}<br />
                 <label class="block">ISBN: </label>
-                <input type='text' class={errors.isbn ? 'text-red-600' : 'text-blue-600'} name='isbn' value={input.isbn} placeholder='isbn' onChange={(e) => handleChange(e)} /><br />
-                {errors.isbn ? <p class="text-red-600">{errors.isbn}</p> : null}<br />
+                <input type='text' class={errors.ISBN ? 'text-[#dc2626]' : 'text-[#075985]'} name='ISBN' value={input.ISBN} placeholder='isbn' onChange={(e) => handleChange(e)} /><br />
+                {errors.ISBN ? <p class="text-[#dc2626]">{errors.ISBN}</p> : null}<br />                               
+                <label class="block">PRECIO: </label>
+                <input type='text' class={errors.price ? 'text-[#dc2626]' : 'text-[#075985]'} name='price' value={input.price} placeholder='Ingrese el precio' onChange={(e) => handleChange(e)} /><br />
+                {errors.price ? <p class="text-[#dc2626]">{errors.price}</p> : null}<br />
+                <label class="block">IMAGEN: </label>
+                <input type='file' name='imageLinks' value={input.imageLinks} placeholder='imagen' onChange={(e) => handleChange(e)} />
+                {errors.imageLinks ? <p class="text-[#dc2626]">{errors.imageLinks}</p> : null}<br />
                 <label class="block">CATEGORIA:</label>
-                <select name="categoria" value={input.categoria} placeholder='categoria' onClick={handleSelect} class={errors.categoria ? 'text-red-600' : 'text-blue-600'} multiple>
+                <select name="categories" value={input.categories} placeholder='categoria' onClick={handleSelect} class={errors.categories ? 'text-[#dc2626]' : 'text-[#075985]'} multiple>
                     <option value='CIENCIA FICCION'>CIENCIA FICCION</option>
                     <option value='COMEDIA'>COMEDIA</option>
                     <option value='DRAMA'>DRAMA</option>
@@ -149,14 +159,14 @@ export default function FormBook() {
                     <option value='ROMANCE'>ROMANCE</option>
                     <option value='SIN CATEGORIA'>SIN CATEGORIA</option>
                 </select>
-                {errors.categoria ? <p class="text-red-600">{errors.categoria}</p> : null}<br />
-                {input.categoria.map(cat => {
+                {errors.categories ? <p class="text-[#dc2626]">{errors.categories}</p> : null}<br />
+                {input.categories.map(cat => {
                     return <span>{cat},</span>
                 })}
             </fieldset>
             <fieldset class="text-center">
                 <legend>DESCRIPCION:</legend>
-                <textarea class="w-full text-blue-600" name="descripcion" value={input.descripcion} onChange={(e) => handleChange(e)} />
+                <textarea class="w-full text-[#075985]" name="description" value={input.description} onChange={(e) => handleChange(e)} />
 
                 <br />
                 {/*loading?<p>{loading}</p>:null*/}
