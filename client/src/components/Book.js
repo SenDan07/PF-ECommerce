5b3a5d8a1f3729a781e99411df3a3588d42153ad
William Garcia <william_garcia_m@outlook.com>
2022-10-04 10:31:39 -0500

    Actualización de Api Books a 60 libros

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -5,18 +5,23 @@ export const Book = ({ name, image, price, id }) => {
   return (
     <div>
       <div>
-        <h2 className="randomName text-center text-[22px]">
+        <h2 className="font-medium text-center text-[22px]">
           {name.length > 19 ? `${name.slice(0, 18)}...` : name}
         </h2>
       </div>
-      <div>
+      <div className="mb-1">
         <Link to={`/books/${id}`}>
-          <img src={image} alt={`img-${name}`} className="h-80 w-64 rounded" />
+          <img
+            src={image}
+            alt={`img-${name}`}
+            className="w-[200px] h-[300px] rounded sombra"
+          />
         </Link>
       </div>
       <div>
-        <h3 className="text-center text-lg font-bold">
-          <span className="font-medium">Price: </span>${price.toFixed(2)}
+        <h3 className="text-center text-xl font-bold text-black">
+          <span className="font-medium text-white">Price: </span>$
+          {price.toFixed(2)}
         </h3>
       </div>
     </div>
--- client/src/components/CategoryBooksDumb.js
+++ client/src/components/CategoryBooksDumb.js
@@ -1,23 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Book } from "./Book";
 
 export default function CategoryBook({ title, imageLinks, price, id }) {
-    console.log("title: ", title)
-    return (
-        <div className="m-5 ">
-            <Link to={`/books/${id}`} >
-                <h2 className="randomName text-center text-[22px]">
-                    {title.length > 19 ? `${title.slice(0, 18)}...` : title}
-                </h2>
-
-                <div className="w-64 h-64">
-                    <img src={imageLinks} alt={`img-${title}`} className="h-80 w-64 rounded" />
-                </div>
-
-                <h3 className="text-center text-lg font-bold flex mt-16 justify-center">
-                    <span className="font-medium  ">Price: </span>${price.toFixed(2)}
-                </h3>
-            </Link>
-        </div>
-    );
-}; 
+  //   console.log("title: ", title);
+  return (
+    <div className="m-5 ">
+      <Book name={title} image={imageLinks} price={price} id={id} />
+    </div>
+  );
+}
