683d4d357e46df0f3801404c92bfb91a1ae1857a
brayanalexander <brayangenesis@gmail.com>
2022-10-05 19:39:05 -0500

    rutas admin books

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -4,6 +4,14 @@ const adminControllers = require("../controllers/adminControllers");
 
 const router = Router();
 
+
+/*------------------- RUTAS PRODUCTO (CRUD) ---------------------- */
+
 router.post("/create-book", adminControllers.createBook);
+
+router.put('/books/:idBook',adminControllers.updateBook)
+
+router.delete('/books/:idBook',adminControllers.deleteBook)
+
 
 module.exports = router;
