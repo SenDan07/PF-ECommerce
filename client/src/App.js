d0358704de410122fa097171e0aa1e60caf3334a
William Garcia <william_garcia_m@outlook.com>
2022-10-05 22:41:25 -0500

    Funcion Logearse y Deslogearse completada

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,27 @@ import { Route, BrowserRouter, Switch, Routes } from "react-router-dom";
 import MenuAdmin from "./components/MenuAdmin";
 import { Login } from "./components/Login";
 import FormUser from "./components/FormUser";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { isLogin } from "./redux/actions";
 
 function App() {
+  const dispatch = useDispatch();
+
   const LOGIN = useSelector((state) => state.login);
   const ROLE = useSelector((state) => state.role);
 
   const activeLogin = localStorage.getItem("LOGIN");
-  // const activeRol = localStorage.getItem("ROLE");
+  const activeRole = localStorage.getItem("ROLE");
+
+  useEffect(() => {
+    dispatch(
+      isLogin({
+        login: activeLogin,
+        role: activeRole,
+      })
+    );
+  }, [activeLogin, activeRole]);
 
   return (
     <div className="App">
@@ -25,9 +38,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/createbook" element={<FormBook />} />
-          {activeLogin == 1 ? null : (
+          {LOGIN == 1 && ROLE === "USER" ? (
             <Route exact path="/favorites" element={<Favorites />} />
-          )}
+          ) : null}
           <Route exact path="/books/:id" element={<BookDetail />} />
           <Route exact path="/searchbar" element={<SearchBarSmart />} />
           <Route
@@ -36,9 +49,9 @@ function App() {
             element={<CategoryBooksSmart />}
           />
           <Route exact path="/categories" element={<CategoriesSmart />} />
-          {activeLogin == 1 ? null : (
+          {LOGIN == 1 && ROLE === "ADMIN" ? (
             <Route exact path="/admin" element={<MenuAdmin />} />
-          )}
+          ) : null}
           {activeLogin == 1 ? null : (
             <Route exact path="/register" element={<FormUser />} />
           )}
