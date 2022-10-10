7d06ba067d8843dc6b3453569ddab752b7b22a75
Diego López B <89640777+Saint-Diego@users.noreply.github.com>
2022-10-10 11:23:08 -0500

    Update loginController.js

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -287,12 +287,12 @@ const loginController = {
       const userfind = models.User.findByPk({
         where: {
           id,
-          status: "active",
+          isActive: true,
         },
         include: [
           {
             model: Books,
-            as: "books",
+            as: "favorites",
             through: {
               attributes: [],
             },
