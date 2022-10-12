872ea9e6c963a37de8241d8570c1d2f138a81e9a
SenDan07 <dany.alekrmz@gmail.com>
2022-10-11 17:07:18 -0700

    modelo review creado

---
+++ server/src/models/Review.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require("sequelize");
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("review", {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+};
