cee996fbe1bad0de9b6e3a57ba07cca209301a74
Saint-Diego <ing.diegolopezb@gmail.com>
2022-10-14 18:30:38 -0500

    Creada las funciones de obtener ordenes en admin

--- server/src/controllers/alertController.js
+++ server/src/controllers/alertController.js
@@ -1,19 +1,18 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const HttpError = require("../errors/http-error");
- const {
-  CLIENT_ID,
-  CLIENT_SECRET,
-   REFRESH_TOKEN,
-  REDIRECT_URI
- } = process.env;
+const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REDIRECT_URI } = process.env;
 
 const alertController = {
   sendEmail: async (req, res, next) => {
     const { emails, subject, content } = req.body;
 
     try {
-      const oAuthsClient = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+      const oAuthsClient = new google.auth.OAuth2(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        REDIRECT_URI
+      );
       oAuthsClient.setCredentials({ refresh_token: REFRESH_TOKEN });
       const accessToken = await oAuthsClient.getAccessToken();
       const transporter = nodemailer.createTransport({
@@ -38,11 +37,27 @@ const alertController = {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <style>
-          h1, h3, h5 {
-            font-family: "Arial";
+          h1, h3, h4 {
+            font-family: Arial, Helvetica, sans-serif;
           }
-          h5 {
-            color: "#808080";
+          h4 {
+            color: #808080;
+          }
+          a {
+            padding: 5px 10px;
+            background: #32bd5c;
+            color: #2e2d2d;
+            font-family: sans-serif, Arial, aouto;
+            font-weight: bold;
+            border: 1px solid #00000075;
+            border-radius: 5px;
+            text-decoration: none;
+          }
+          a:hover {
+            background: #2e2d2d;
+            color: #32bd5c;
+            cursor: pointer;
+            border: 2px solid #32bd5c;
           }
         </style>
         <title>Librería PF</title>
@@ -61,12 +76,12 @@ const alertController = {
       const info = await transporter.sendMail(mailDetails);
       res.status(201).send(info);
     } catch (error) {
+      console.log(error);
       if (!(error instanceof HttpError)) {
         error = new HttpError("Error de credenciales o token expirado", 400);
       }
       return next(error);
     }
-
   },
 };
 
