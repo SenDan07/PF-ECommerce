8f9ba82ee6079d56b1233bb57f9d972d7b58ea91
William Garcia <william_garcia_m@outlook.com>
2022-10-13 20:05:29 -0500

    Se arreglaron bugs

--- server/src/models/Order.js
+++ server/src/models/Order.js
@@ -1,23 +1,27 @@
-const { DataTypes }=require('sequelize');
+const { DataTypes } = require("sequelize");
 
-module.exports=(sequelize)=>{
-    sequelize.define('order',{
-        direccion: {
-			type: DataTypes.TEXT,
-			allowNull: false
-		},
-        telefono:{
-            type: DataTypes.STRING,
-			allowNull: false
-        },
-        pais:{
-            type: DataTypes.STRING,
-			allowNull: false
-        },
-		total: {
-			type: DataTypes.FLOAT,
-		}
-	 },{
-        timestamps:false,
-    });
-} 
+module.exports = (sequelize) => {
+  sequelize.define(
+    "order",
+    {
+      direccion: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      telefono: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      pais: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      total: {
+        type: DataTypes.FLOAT,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+};
